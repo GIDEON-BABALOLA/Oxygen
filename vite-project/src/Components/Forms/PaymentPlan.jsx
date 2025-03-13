95444db06c9f99f1227eef8822a9f95eaaa32255
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-03-13 13:44:24 +0100

    almost done

--- vite-project/src/Components/Forms/PaymentPlan.jsx
+++ vite-project/src/Components/Forms/PaymentPlan.jsx
@@ -4,24 +4,32 @@ const PaymentPlan = () => {
   return (
     <div className='payment-plan'>
   <div style={{color : "#000000", fontSize : "24px", fontWeight : 700}}>Payment Plan</div>
-<div style={{backgroundColor : "#39B7E933", display :"flex", flexDirection : "row", padding :"10px 10px", borderRadius : "5px"}}>
-<section style={ {display : "flex", flexDirection : "row", justifyContent : "space-between", width : "100%"}}>
-    <div>
-<span>Loan Amount</span>
-<span>N150,000</span>
+<div style={{backgroundColor : "#39B7E933", display :"flex", flexDirection : "column", padding :"40px 40px", borderRadius : "5px", 
+justifyContent : "space-between",
+gap : "100px",
+}}>
+<section
+style={ {display : "flex", flexDirection : "row", justifyContent : "space-between", width : "100%"}}>
+    <div className='loan-options'>
+<span style={{fontSize : "16px"}}>Loan Amount</span>
+<span className='loan-specifics'>N150,000</span>
     </div>
-    <div>
-    <span>Repayment Date</span>
-    <span>31-07-2024</span>
+    <div  className='loan-options'>
+    <span style={{fontSize : "16px"}} > Repayment Date</span>
+    <span className='loan-specifics'>31-07-2024</span>
     </div>
 </section>
-<section>
-<div>
+<section style={ {display : "flex", flexDirection : "row", justifyContent : "space-between", width : "100%"}}>
+<div className='loan-options'>
+<span style={{fontSize : "16px"}}>Interest Rate</span>
+<span className='loan-specifics'>12.5%</span>
+    </div>
+    <hr class="styled-line" />
 
-</div>
-<div>
-    
-</div>
+    <div  className='loan-options'>
+    <span style={{fontSize : "16px"}}>Loan Repayment</span>
+    <span className='loan-specifics'>N24,000</span>
+    </div>
 </section>
 </div>
     </div>
