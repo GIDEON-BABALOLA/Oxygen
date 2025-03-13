3001e320a521b2cd86ce36650bce0005f98407dd
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-03-13 13:23:52 +0100

    still working

--- vite-project/src/Components/Forms/GetALoan.jsx
+++ vite-project/src/Components/Forms/GetALoan.jsx
@@ -9,7 +9,7 @@ const GetALoan = () => {
         You repayment on a 
         <span style={{fontSize : "32px", color : "#39B7E9", fontWeight : 700}}> N150,000</span> loan is <span style={{fontSize : "32px", color : "#39B7E9", fontWeight : 700}}> N24,000</span> per month for<span style={{fontSize : "32px", color : "#39B7E9", fontWeight : 700}}> 6 months</span>
         </div>
-        <div style={{textDecoration : "underline", color : "#39B7E9"}}>Tap here to see full details</div>
+        <div style={{textDecoration : "underline", color : "#39B7E9", cursor  :"pointer"}}>Tap here to see full details</div>
     </div>
         <div></div>
     </section>
