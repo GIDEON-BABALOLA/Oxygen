fe8eb9aa6b52eef2d4ade2d4e4b30b466ca4f94f
GIDEON-BABALOLA <gideonbabalola69@gmail.com>
2025-03-13 13:23:17 +0100

    still working

--- vite-project/src/Components/Forms/GetALoan.jsx
+++ vite-project/src/Components/Forms/GetALoan.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 const GetALoan = () => {
   return (
-    <div className='get-a-loan'>
+    <section style={{display : "flex", flexDirection : "column", gap : "90px"}}>
+            <div className='get-a-loan'>
         <div style={{color : "#000000", fontSize : "24px", fontWeight : 700}}>Payment Plan</div>
-        <div>
-        You repayment on a N150,000 loan is N24,000 per month for 6 months
+        <div style={{fontSize : "20px"}}>
+        You repayment on a 
+        <span style={{fontSize : "32px", color : "#39B7E9", fontWeight : 700}}> N150,000</span> loan is <span style={{fontSize : "32px", color : "#39B7E9", fontWeight : 700}}> N24,000</span> per month for<span style={{fontSize : "32px", color : "#39B7E9", fontWeight : 700}}> 6 months</span>
         </div>
-        <div style={{textDecoration : "underline"}}>Tap here to see full details</div>
+        <div style={{textDecoration : "underline", color : "#39B7E9"}}>Tap here to see full details</div>
     </div>
+        <div></div>
+    </section>
+
   )
 }
 
---
+++ vite-project/src/Components/Forms/PaymentPlan.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PaymentPlan = () => {
+  return (
+    <div className='payment-plan'>
+  <div style={{color : "#000000", fontSize : "24px", fontWeight : 700}}>Payment Plan</div>
+
+    </div>
+  )
+}
+
+export default PaymentPlan
